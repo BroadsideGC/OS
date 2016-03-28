e20cf7004caacd9ccf50913f51df898172b79aef
Kirik64rus <kzyemskov@mail.ru>
2016-03-28 20:31:03 +0300

    Finally block works

--- sigusr/sigusr.c
+++ sigusr/sigusr.c
@@ -6,21 +6,23 @@
 #include <sys/types.h>
 #include <errno.h>
 
+volatile sig_atomic_t sig_status = 0;
+
 void sig_handler(int signo, siginfo_t *siginfo, void *context)
 {
-
   pid_t pid = siginfo->si_pid;
-  printf("%s from %lu\n", (signo == SIGUSR1 ? "SIGUSR1" : "SIGUSR2"), (unsigned long) pid);
+  if (sig_status == 0){
+      sig_status = 1;
+      printf("%s from %lu\n", (signo == SIGUSR1 ? "SIGUSR1" : "SIGUSR2"), (unsigned long) pid);
+  }
+  sleep(10);
   exit(signo);
 }
 
 int main(void){
   struct sigaction sa;
   sa.sa_sigaction = *sig_handler;
-  sa.sa_flags = SA_SIGINFO;
-  sigset_t set;
-  sigfillset(&set);
-  sa.sa_mask = set;
+  sa.sa_flags |= SA_SIGINFO;
   if (sigaction(SIGUSR1, &sa, NULL) != 0  || sigaction(SIGUSR2, &sa, NULL) != 0){
     printf("error\n");
     return errno;
