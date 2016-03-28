1f475ca25ba6c6710fd94eab8dd76c44eaded895
Kirik64rus <kzyemskov@mail.ru>
2016-03-28 17:13:59 +0300

    sigusr fixes

--- sigusr/sigusr.c
+++ sigusr/sigusr.c
@@ -3,24 +3,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
 #include <errno.h>
 
 void sig_handler(int signo, siginfo_t *siginfo, void *context)
 {
-  if (signo == SIGUSR1){
-    printf("SIGUSR1 from %d\n", (int)siginfo->si_pid);
-  }else{
-    printf("SIGUSR2 from %d\n", (int)siginfo->si_pid);
-  }
+
+  pid_t pid = siginfo->si_pid;
+  printf("%s from %lu\n", (signo == SIGUSR1 ? "SIGUSR1" : "SIGUSR2"), (unsigned long) pid);
   exit(signo);
 }
 
 int main(void){
   struct sigaction sa;
   sa.sa_sigaction = *sig_handler;
+  sa.sa_flags = SA_SIGINFO;
+  sigset_t set;
+  sigfillset(&set);
+  sa.sa_mask = set;
   if (sigaction(SIGUSR1, &sa, NULL) != 0  || sigaction(SIGUSR2, &sa, NULL) != 0){
     printf("error\n");
-     return errno;
+    return errno;
   }
   sleep(10);
   printf("No signals were caught\n");
